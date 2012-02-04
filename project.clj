d1385164755e227bc9848492cbb1016485cdc391
Michael S. Klishin <michael@novemberain.com>
2012-02-04 05:39:26 +0400

    Upgrade to clj-http 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject clojurewerkz/crawlista "1.0.0-SNAPSHOT"
   :description "Support library for Clojure applications that crawl the Web"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http            "0.2.4"]
+                 [clj-http            "0.3.1-SNAPSHOT"]
                  [org.jsoup/jsoup     "1.6.1"]
                  [clojurewerkz/urly   "1.0.0-SNAPSHOT"]]
   :multi-deps {
                "1.4" [[org.clojure/clojure "1.4.0-beta1"]]
-               :all [[clj-http            "0.2.4"]
+               :all [[clj-http            "0.3.1-SNAPSHOT"]
                      [org.jsoup/jsoup     "1.6.1"]
                      [clojurewerkz/urly   "1.0.0-SNAPSHOT"]]
                }
