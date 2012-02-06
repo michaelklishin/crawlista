b65d142bd15f60dfd41c5155b18a08e72f2c2425
Michael S. Klishin <michael@novemberain.com>
2012-02-06 12:45:31 +0400

    More test cases

--- test/clojurewerkz/crawlista/test/robots.clj
+++ test/clojurewerkz/crawlista/test/robots.clj
@@ -18,5 +18,8 @@
                    # и еще один"))))
 
 (deftest test-parsing-of-input-with-just-a-user-agent-string
-  (is (= { "webcrawler" [] } (parse "User-agent: webcrawler"))))
+  (is (= { "webcrawler" [] } (parse "User-agent: webcrawler")))
+  (is (= { "webcrawler" [] } (parse "User-agent: webcrawler # a comment")))
+  (is (= { "webcrawler" [] } (parse "User-agent: webcrawler # a comment
+                                     # и еще один комментарий"))))
 
