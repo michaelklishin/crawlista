ad923027696d8d4fd911c289c21586ad11815c8c
Michael S. Klishin <michael@novemberain.com>
2012-02-06 20:20:36 +0400

    Several new test cases for User-agent string parsing (including invalid ones taken from real world robots.txt files)

--- test/clojurewerkz/crawlista/test/robots.clj
+++ test/clojurewerkz/crawlista/test/robots.clj
@@ -22,5 +22,17 @@
   (is (= { "webcrawler" [] } (parse "User-agent: webcrawler  ")))  
   (is (= { "webcrawler" [] } (parse "User-agent: webcrawler # a comment")))
   (is (= { "webcrawler" [] } (parse "User-agent: webcrawler # a comment
-                                     # и еще один комментарий"))))
+                                     # и еще один комментарий")))
+  (is (= { "*" [] }                (parse "User-agent: *")))
+  (is (= { "Googlebot" [] }        (parse "User-agent: Googlebot")))
+  (is (= { "Googlebot-Mobile" [] } (parse "User-agent: Googlebot-Mobile")))
+  (is (= { "baiduspider" [] }      (parse "User-agent: baiduspider")))
+  (is (= { "msnbot" [] }           (parse "User-agent: msnbot # MSN, who uses dat.")))
+  (is (= { "Oracle Secure Enterprise Search" [] }                (parse "User-agent: Oracle Secure Enterprise Search # must be some serious shit")))
+  (is (= { "EDI/1.6.0 (Edacious & Intelligent Web Crawler)" [] } (parse "User-agent: EDI/1.6.0 (Edacious & Intelligent Web Crawler)")))
+  (is (= { "Microsoft-WebDAV-MiniRedir/5.1.2600" [] }            (parse "User-agent: Microsoft-WebDAV-MiniRedir/5.1.2600  ")))
+  (is (= { "MS Search 5.0 Robot" [] }                            (parse "User-agent: MS Search 5.0 Robot")))
+  (is (= { "Python-urllib/1.17" [] }                             (parse "User-agent: Python-urllib/1.17")))
+  (is (= { "Xenu Link Sleuth" [] }                               (parse "User-agent: Xenu Link Sleuth")))
+  (is (= { "Mediapartners-Google*" [] }                          (parse "User-agent: Mediapartners-Google*"))))
 
