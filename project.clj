34459748f93f56d3d6ecc5e03d60fa611417330e
Michael S. Klishin <michael@novemberain.com>
2012-03-05 08:45:22 +0400

    Don't depend on snapshots

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject clojurewerkz/crawlista "1.0.0-alpha1"
   :description "Support library for Clojure applications that crawl the Web"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http            "0.3.1-SNAPSHOT"]
+                 [clj-http            "0.3.2"]
                  [org.jsoup/jsoup     "1.6.1"]
                  [clojurewerkz/urly   "1.0.0-beta2"]]
   :multi-deps {
                "1.4" [[org.clojure/clojure "1.4.0-beta2"]]
-               :all [[clj-http            "0.3.1-SNAPSHOT"]
+               :all [[clj-http            "0.3.2"]
                      [org.jsoup/jsoup     "1.6.1"]
                      [clojurewerkz/urly   "1.0.0-beta2"]]
                }
