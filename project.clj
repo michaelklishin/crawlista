8ab67ded83372fab5bbdce40ea143e3078ef99c0
Michael S. Klishin <michael@novemberain.com>
2012-03-13 21:57:37 +0400

    clj-http 0.3.3, profile.clj cleanup after Leiningen 2 migration

--- project.clj
+++ project.clj
@@ -2,14 +2,11 @@
   :description "Support library for Clojure applications that crawl the Web"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http "0.3.2"]
+                 [clj-http "0.3.3"]
                  [org.jsoup/jsoup "1.6.1"]
                  [clojurewerkz/urly "1.0.0-beta2"]]
   :source-paths ["src/clojure"]
   :profiles {:dev {:resource-paths ["test/resources"]},
-             :all {:dependencies [[clj-http "0.3.2"]
-                                  [org.jsoup/jsoup "1.6.1"]
-                                  [clojurewerkz/urly "1.0.0-beta2"]]},
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta4"]]}}
   :aliases { "all" ["with-profile" "dev:dev,1.4"] }
   :resource-paths ["src/resources"]
