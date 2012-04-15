694858c34e1c935ac7f5b953ae2bf19d041aead4
Michael S. Klishin <michael@novemberain.com>
2012-04-15 19:31:50 +0400

    Clojure 1.4.0

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :resource-paths     ["src/resources"]
   :java-source-paths  ["src/java"]
   :profiles           {:dev {:resource-paths ["test/resources"]}
-                       :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta7"]]}}
+                       :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :aliases            { "all" ["with-profile" "dev:dev,1.4"]}
   :warn-on-reflection true
   :test-selectors     {:default    (constantly true)
