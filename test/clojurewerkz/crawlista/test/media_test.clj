18e589ce059132ebcc7f0397c834ed8d86fd3f55
Michael S. Klishin <michael@novemberain.com>
2012-04-26 15:42:05 +0400

    Move example PNG to a machine that is known to respond w/o issues

--- test/clojurewerkz/crawlista/test/media_test.clj
+++ test/clojurewerkz/crawlista/test/media_test.clj
@@ -32,7 +32,7 @@
        "http://readwriteweb.com"                                             true
        "http://clojuredocs.org/"                                             true
        "http://upload.wikimedia.org/wikipedia/en/1/1a/Clojure-glyph.svg"     false
-       "http://upload.wikimedia.org/wikipedia/commons/9/9a/PNG_transparency_demonstration_2.png" false
-       "http://creativecommons.org/images/deed/cc-logo.jpg" false
-       "http://elpais.com.uy/formatos/rss/index.asp?seccion=umomento" false
+       "http://files.novemberain.com/images/png/transparent_png.png"         false
+       "http://creativecommons.org/images/deed/cc-logo.jpg"                  false
+       "http://elpais.com.uy/formatos/rss/index.asp?seccion=umomento"        false
        "http://tagesanzeiger.ch/panorama/vermischtes/stichwort.rss?tagId=1280" false))
