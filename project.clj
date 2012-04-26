ae05f377d4cd50827c61285001aee43b96787986
Michael S. Klishin <michael@novemberain.com>
2012-04-26 13:25:30 +0400

    Add boilerpipe dependency

--- project.clj
+++ project.clj
@@ -1,21 +1,27 @@
 (defproject clojurewerkz/crawlista "1.0.0-alpha14-SNAPSHOT"
   :description "Support library for Clojure applications that crawl the Web"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure       "1.3.0"]
-                 [clj-http                  "0.3.6"]
-                 [org.jsoup/jsoup           "1.6.2"]
-                 [clojurewerkz/urly         "1.0.0"]
-                 [com.novemberain/pantomime "1.3.0"]]
+  :dependencies [[org.clojure/clojure          "1.3.0"]
+                 [clj-http                     "0.3.6"]
+                 [org.jsoup/jsoup              "1.6.2"]
+                 [clojurewerkz/urly            "1.0.0"]
+                 [com.novemberain/pantomime    "1.3.0"]
+                 [de.l3s.boilerpipe/boilerpipe "1.2.0"]]
   :source-paths       ["src/clojure"]
   :resource-paths     ["src/resources"]
   :java-source-paths  ["src/java"]
   :profiles           {:dev {:resource-paths ["test/resources"]}
-                       :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
-  :aliases            { "all" ["with-profile" "dev:dev,1.4"]}
-  :repositories {"clojure-releases" "http://build.clojure.org/releases"
+                       :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+                       :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :aliases        {"ci"  ["with-profile" "dev:dev,1.4"]
+                   "all" ["with-profile" "dev:dev,1.4:dev,1.5"]}  
+  :repositories {"boilerpipe" "http://boilerpipe.googlecode.com/svn/repo/"
                  "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
-                             :releases {:checksum :fail :update :always}}}
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
   :warn-on-reflection true
   :test-selectors     {:default    (constantly true)
                        :focus      :focus
